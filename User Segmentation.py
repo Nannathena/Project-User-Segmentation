950a45969bf97f7a725d058a0ccce8cacc545849
unknown <bayudewantoro9p@gmail.com>
2025-09-02 19:12:05 +0700

    Second Commit

--- User Segmentation.py
+++ User Segmentation.py
@@ -6,10 +6,43 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('Salinan Salinan Online Retail Data.csv', header =0)
 
+print(df)
+
 # Data cleansing
 df_clean = df.copy()
 df_clean['date'] = pd.to_datetime(df_clean['order_date']).dt.date.astype('datetime64[ns]')
-df_clean['amount'] = df_clean['quantity']*df_clean['price']
+
+# menghapus semua baris tanpa customer_id
+df_clean = df_clean[~df_clean['customer_id'].isna()]
+# menghapus semua baris tanpa product_name
+df_clean = df_clean[~df_clean['product_name'].isna()]
+# membuat semua product_name berhuruf kecil
+df_clean['product_name'] = df_clean['product_name'].str.lower()
+# menghapus semua baris dengan product_code atau product_name test
+df_clean = df_clean[(~df_clean['product_code'].str.lower().str.contains('test')) |
+                    (~df_clean['product_name'].str.contains('test '))]
+# membuat kolom order_status dengan nilai 'cancelled' jika order_id diawali dengan huruf 'c' dan 'delivered' jika order_id tanpa awalan huruf 'c'
+df_clean['order_status'] = np.where(df_clean['order_id'].str[:1]=='C', 'cancelled', 'delivered')
+# mengubah nilai quantity yang negatif menjadi positif karena nilai negatif tersebut hanya menandakan order tersebut cancelled
+df_clean['quantity'] = df_clean['quantity'].abs()
+# menghapus baris dengan price bernilai negatif
+df_clean = df_clean[df_clean['price']>0]
+# membuat nilai amount, yaitu perkalian antara quantity dan price
+df_clean['amount'] = df_clean['quantity'] * df_clean['price']
+# mengganti product_name dari product_code yang memiliki beberapa product_name dengan salah satu product_name-nya yang paling sering muncul
+most_freq_product_name = df_clean.groupby(['product_code','product_name'], as_index=False).agg(order_cnt=('order_id','nunique')).sort_values(['product_code','order_cnt'], ascending=[True,False])
+most_freq_product_name['rank'] = most_freq_product_name.groupby('product_code')['order_cnt'].rank(method='first', ascending=False)
+most_freq_product_name = most_freq_product_name[most_freq_product_name['rank']==1].drop(columns=['order_cnt','rank'])
+df_clean = df_clean.merge(most_freq_product_name.rename(columns={'product_name':'most_freq_product_name'}), how='left', on='product_code')
+df_clean['product_name'] = df_clean['most_freq_product_name']
+df_clean = df_clean.drop(columns='most_freq_product_name')
+# mengkonversi customer_id menjadi string
+df_clean['customer_id'] = df_clean['customer_id'].astype(str)
+# menghapus outlier
+from scipy import stats
+df_clean = df_clean[(np.abs(stats.zscore(df_clean[['quantity','amount']]))<3).all(axis=1)]
+df_clean = df_clean.reset_index(drop=True)
+df_clean
 
 # Agregat data transaksi ke bentuk summary total transaksi (order), total nilai order (order value)
 # tanggal order terakhir dari setiap penggunanya
